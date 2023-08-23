82cbbefb23297a7779f1abab529489a443ad09a4
guimagalhaes33 <guimagalhaes343@gmail.com>
2023-08-22 21:14:10 -0300

    calculadora

---
+++ calculadora.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int valor1;
+    int valor2;
+    int resultado;
+    char
+    valor1 = 10;
+    valor2 = 2;
+    // soma: resultado = valor1 + valor2;
+    // subtracao: resultado = valor1 - valor2;
+    // multiplicacao: resultado = valor1 * valor2;
+    // divisao: resultado = valor1 / valor2;
+    printf("resultado : %d\n", resultado);
+
+    return 0;
+}
